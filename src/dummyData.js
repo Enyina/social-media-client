a51ccfc7e56b2c8b127c280e1c10d9fb4e8610ac
Enyina Matthew <enyina3848@gmail.com>
2023-04-21 01:13:32 +0100

    add data

--- src/dummyData.js
+++ src/dummyData.js
@@ -1,53 +1,53 @@
 export const Users = [
   {
-    id:1,
+    id: 1,
     profilePicture: "person/1.jpeg",
-    username: "Safak Kocaoglu",
+    username: " Jhon Wick",
   },
   {
-    id:2,
+    id: 2,
     profilePicture: "person/2.jpeg",
-    username: "Janell Shrum",
+    username: "Angry Bird",
   },
   {
-    id:3,
+    id: 3,
     profilePicture: "person/3.jpeg",
-    username: "Alex Durden",
+    username: "Miss Boo",
   },
   {
-    id:4,
+    id: 4,
     profilePicture: "person/4.jpeg",
-    username: "Dora Hawks",
+    username: "Dada Hawkson",
   },
   {
-    id:5,
+    id: 5,
     profilePicture: "person/5.jpeg",
-    username: "Thomas Holden",
+    username: "Thomas shelden",
   },
   {
-    id:6,
+    id: 6,
     profilePicture: "person/6.jpeg",
-    username: "Shirley Beauchamp",
+    username: "Young Blood",
   },
   {
-    id:7,
+    id: 7,
     profilePicture: "person/7.jpeg",
-    username: "Travis Bennett",
+    username: "Travis Scott",
   },
   {
-    id:8,
+    id: 8,
     profilePicture: "person/8.jpeg",
-    username: "Kristen Thomas",
+    username: "You Ming",
   },
   {
-    id:9,
+    id: 9,
     profilePicture: "person/9.jpeg",
-    username: "Gary Duty",
+    username: "Gary Dem",
   },
   {
-    id:10,
+    id: 10,
     profilePicture: "person/10.jpeg",
-    username: "Safak Kocaoglu",
+    username: "Yoland Khan",
   },
 ];
 
